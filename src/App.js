891dbc9996da7eb2e8eefe7193b067c534723c9b
junaidsami <junaidsami269@gmail.com>
2022-10-22 20:38:03 +0500

    latest

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Create from "./Create";
 import BlogDetail from "./BlogDetail";
+import NotFound from "./NotFound";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home/>}/>  
           <Route path="/create" element={<Create/>}/>  
           <Route path="/blogs/:id" element={<BlogDetail/>}/>
+          <Route path="*" element={<NotFound/>}/>
           
         </Routes>
       </div>
